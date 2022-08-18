670f76d286dfd57f6bb5fa2709becaa4360a48da
Armin0001 <59747818+Armin0001@users.noreply.github.com>
2022-08-18 18:05:46 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+<script type="text/javascript">
+        var current= new Date()
+var day_night=current.getHours()
+if (day_night<=12)
+    document.write("<img src='morning.jpg'>")
+else
+    document.write("<img src='night.jpg'>")
+</script>
